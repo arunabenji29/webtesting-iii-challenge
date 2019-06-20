2dae844ca65406668e6adc2be143bbeb2892bc37
Aruna Benjamin <arunabenji29@gmail.com>
2019-06-19 21:12:38 -0600

    testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,50 @@
 // Test away!
+import React from 'react'
+import {render,fireEvent,getByTestId} from '@testing-library/react'
+
+import 'jest-dom/extend-expect'
+import Controls from './Controls'
+import Dashboard from '../dashboard/Dashboard'
+import '@testing-library/react/cleanup-after-each'
+
+// describe('<Controls />', () => {
+//     it('close gate button clicked', () => {
+        
+//         const container = document.body
+
+//         const {getByText} = render(<Dashboard/>);
+
+//         const button = getByText('Close Gate')
+
+//         fireEvent.click(button)
+
+//         expect(getByTestId(container, /close/i).textContent).toBe('Closed')
+//         expect(getByTestId(container, /lock/i).textContent).toBe('Unlocked')
+//         expect(document.querySelector('[data-testid="access"]')).not.toBeDisabled()
+//         expect(getByTestId(container, /position/i).textContent).toBe('Open Gate')
+        
+
+//     })
+// })
+
+describe('<Controls second test/>', () => {
+
+    it('open gate button clicked', () => {
+        
+        const container = document.body
+
+        const {getByText} = render(<Controls closed={false}/>);
+
+        // const button = getByText('Open Gate')
+
+        console.log('button text ',getByText(/Open Gate/i))
+        // fireEvent.click(button)
+
+        // expect(getByTestId(container, /close/i).textContent).toBe('Open')
+        // expect(getByTestId(container, /lock/i).textContent).toBe('Unlocked')
+        // expect(document.querySelector('[data-testid="access"]')).toBeDisabled()
+        // expect(getByTestId(container, /position/i).textContent).toBe('Close Gate')
+        
+
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from 'react'
+import {render} from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
+
+import Display from './Display'
+import Dashboard from '../dashboard/Dashboard'
+
+describe('<Display />', () => {
+    it('should show unlocked and open', () => {
+        const show={
+            position:'Unlocked',
+            door:'Open',
+        }
+
+        const {getByTestId} = render(<Display show={show}/>);
+
+        const renderedLock = getByTestId(/lock/i)
+
+        const renderedClose = getByTestId(/close/i)
+
+        // console.log('render lock ',renderedLock)
+        // console.log('render close ',renderedClose)
+
+        // console.log('show position ',show.position)
+        // console.log('show door ',show.door)
+
+        expect(renderedLock.textContent).toBe(`${show.position}`)
+
+        expect(renderedClose.textContent).toBe(`${show.door}`)
+
+    })
+})
